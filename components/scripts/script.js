b9885925a520165fada6e61abe0aeb557a77c413
Vitaliy Krushinsky <vitaliykrushinsky@gmail.com>
2015-08-08 16:43:23 +0300

    Vertical centered items in room section and added fullheight view

---
+++ components/scripts/script.js
@@ -0,0 +1,11 @@
+$(function() {
+	var wheight = $(window).height(); // get height of the window
+	
+	$('.fullheight').css('height', wheight);
+	
+	$(window).resize(function() {
+		var wheight = $(window).height(); // get height of the window
+	
+		$('.fullheight').css('height', wheight);
+	});
+});
