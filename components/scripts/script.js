daf8b62e0431a891371b40980212184a2bdc3cd1
Vitaliy Krushinsky <vitaliykrushinsky@gmail.com>
2015-08-26 15:28:30 +0300

    Highlighted menu items on scroll

--- components/scripts/script.js
+++ components/scripts/script.js
@@ -13,24 +13,56 @@ $(function() {
 	}); //on resize
 	
 //Snippet to performs a smooth page scroll to an anchor on the same page.
-	 //Use smooth scrolling when clicking on navigation
-  $('.navbar a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') === 
-      this.pathname.replace(/^\//,'') && 
-      location.hostname === this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top-topoffset
-        }, 500);
-        return false;
-      } //target.length
-    } //click function
-  }); //smooth scrolling
+	//Use smooth scrolling when clicking on navigation
+	$('.navbar a[href*=#]:not([href=#])').click(function() {
+	if (location.pathname.replace(/^\//,'') === 
+	  this.pathname.replace(/^\//,'') && 
+	  location.hostname === this.hostname) {
+	  var target = $(this.hash);
+	  target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	  if (target.length) {
+		$('html,body').animate({
+		  scrollTop: target.offset().top-topoffset
+		}, 500);
+		return false;
+	  } //target.length
+	} //click function
+	}); //smooth scrolling
+
+	
+//Highlighting menu items on scroll
+	$(window).scroll(function() {
+		var windowpos = $(window).scrollTop() + topoffset;
+		$('nav li a').removeClass('active');
+		
+		if (windowpos > $('#hotelinfo').offset().top) {
+			$('nav li a').removeClass('active');
+			$('a[href$=#hotelinfo]').addClass('active');
+		}
+		
+		if (windowpos > $('#rooms').offset().top) {
+			$('nav li a').removeClass('active');
+			$('a[href$=#rooms]').addClass('active');
+		}
+		
+		if (windowpos > $('#dining').offset().top) {
+			$('nav li a').removeClass('active');
+			$('a[href$=#dining]').addClass('active');
+		}
+		
+		if (windowpos > $('#events').offset().top) {
+			$('nav li a').removeClass('active');
+			$('a[href$=#events]').addClass('active');
+		}
+		
+		if (windowpos > $('#attractions').offset().top) {
+			$('nav li a').removeClass('active');
+			$('a[href$=#attractions]').addClass('active');
+		}
+	});
 	
 	
-//on set up Scrollmagic	and TweenMax
+//Set up Scrollmagic and TweenMax
 	var controller = new ScrollMagic({
 		globalSceneOptions: {
 			triggerHook: "onLeave"
