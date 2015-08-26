e7dc041b47abf39c85514a8fb87cfdde6e44f646
Vitaliy Krushinsky <vitaliykrushinsky@gmail.com>
2015-08-26 10:03:00 +0300

    Set up ScrollMagic and set up handled animation by TweenMax for attraction section

--- components/scripts/script.js
+++ components/scripts/script.js
@@ -1,4 +1,5 @@
 $(function() {
+	
 	var wheight = $(window).height(); // get height of the window
 	
 	$('.fullheight').css('height', wheight);
@@ -7,5 +8,21 @@ $(function() {
 		var wheight = $(window).height(); // get height of the window
 	
 		$('.fullheight').css('height', wheight);
+	}); //on resize
+	
+	
+	var controller = new ScrollMagic({
+		globalSceneOptions: {
+			triggerHook: "onLeave"
+		}
 	});
+	
+	var attractionstween = TweenMax.staggerFromTo('#attractions article', 1, { opacity: 0, scale: 0 }, { delay: 1, opacity: 1, scale: 1, ease: Back.easeInOut });
+	
+	var scene = new ScrollScene({
+		triggerElement: '#attractions'
+	}).setTween(attractionstween)
+	  .addTo(controller);
+	
+	
 });
